a17f46ac24a1f74ba368de93278dd57a089d5676
Sohteum <thtwk1123@gmail.com>
2023-08-25 18:56:47 +0900

    .

---
+++ 04 styledComponents/src/App.js
@@ -0,0 +1,34 @@
+import { styled } from "styled-components";
+
+function App() {
+
+  const Father = styled.div`
+  display:flex;
+  `;
+
+  const Box = styled.div`
+  background-color:${(props)=> props.bgColor};
+  width: 100px;
+  height: 100px;
+  `
+
+  const Circle = styled(Box)`
+  border-radius:50px;
+  `
+
+  // const Text = styled.span`
+  // color: white;
+  // `;
+
+  return (
+    <>
+      <Father>
+        <Box bgColor="teal"></Box>
+        <Circle bgColor="whiteSmoke"></Circle>
+
+      </Father>
+    </>
+  );
+}
+
+export default App;
