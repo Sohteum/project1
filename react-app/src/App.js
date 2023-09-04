a3214bfb554b5a445540e368a3d3bed93d270d4f
Sohteum <thtwk1123@gmail.com>
2023-09-04 17:33:10 +0900

    .

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,31 @@
+import { useEffect } from "react";
+import CompMenu from "./components/CompMenu";
+import axios from "axios";
+
 function App() {
+
+  axios({
+    url: 'https://www.thecocktaildb.com/api/json/v1/1/random.php', // 통신할 웹문서
+    method: 'get', // 통신할 방식
+    data: { // 인자로 보낼 데이터
+      foo: 'diary'
+    }
+  });
+
   return (
     <>
-    app
+      <header >
+        coctail
+      </header>
+      <main>
+        <input type="text" />
+        <button>칵테일검색하기</button>
+        <CompMenu />
+        <div></div>
+      </main>
+      <footer>
+        카피라이트 뭐시기
+      </footer>
     </>
   );
 }
---
+++ react-app/src/components/CompMenu.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CompMenu = () => {
+    return (
+        <div>
+          <div>
+          <button className='recommandMenu'>추천칵테일</button>
+          <div>칵테일나열</div>
+          </div> 
+        </div>
+    );
+};
+
+export default CompMenu;
