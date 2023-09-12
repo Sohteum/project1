f70ccd359e4648759f53299d224ddd9b87edb76d
Sohteum <thtwk1123@gmail.com>
2023-09-12 11:37:52 +0900

    .

--- react-app/src/PokemonList.js
+++ react-app/src/PokemonList.js
@@ -1,34 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
-import PokemonDetail from './PokemonDetail';
 
 const PokemonList = () => {
 
+
   const [pokemon, setPokemon] = useState({});
   const [loading, setLoading] = useState(true)
-  const [page, setPage] = useState(true)
+  // const [page, setPage] = useState(true)
 
   useEffect(() => {
     setLoading(true)
     let cancel
-    axios.get(`https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`).then(res => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon`).then(res => {
       setLoading(false)
-      setPokemon(res.data)
+      setPokemon(res.data.results(p=>p.name))
     })
 
     return () => cancel()
-  }, [page])
+  }, [])
 
 
   if (loading) return "Loading..."
 
   return (
     <div>
-       {pokemon.results ? pokemon.results.map((pokemon, index) => (
-              <div key = {index}>
-                <PokemonDetail pokemon = {pokemon} />
-              </div>
-     )) : <h2 style = {{margin: "0 auto"}}>Loading</h2>}
+       {pokemon ? <div key={p}>{p}</div> : <div>loading</div>}
     </div>
   );
 
